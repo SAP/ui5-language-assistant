954dc2d51e339826eb16e900593043a5f0bcc7a5
I060847 <shachar.soel@sap.com>
2020-02-04 11:57:20 +0200

    docs: uI5Class early version

--- packages/semantic-model/api.d.ts
+++ packages/semantic-model/api.d.ts
@@ -5,10 +5,33 @@ export interface SemanticModel {
   typedefs: Record<string, UI5Types>;
 }
 
-export interface UI5Class {}
+export interface UI5Class {
+  // TODO: FQN needed? or is this only on the symbol table
+  name: string;
+  // TODO: Parent Namespace ref needed?
+  extends: UI5Class;
+  implements: UI5Interface[];
+  constructor: UI5Method;
+  properties: UI5Prop[];
+  aggregations: UI5Aggregation[];
+  associations: UI5Association[];
+  events: UI5Event[];
+}
 
 export interface UI5Enum {}
 
 export interface UI5Namespace {}
 
 export interface UI5Types {}
+
+export interface UI5Method {}
+
+export interface UI5Prop {}
+
+export interface UI5Aggregation {}
+
+export interface UI5Association {}
+
+export interface UI5Event {}
+
+export interface UI5Interface {}
