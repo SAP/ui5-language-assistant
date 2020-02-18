7c89e77ab50b6fcc685403b147f05e6d7487b9ca
I060847 <shachar.soel@sap.com>
2020-02-18 16:02:05 +0200

    docs: add comments and refs for API matches Imple type assertions

--- packages/logic-utils/test/implementation-matches-public-api.ts
+++ packages/logic-utils/test/implementation-matches-public-api.ts
@@ -1,9 +1,14 @@
 import * as publicApi from "../api";
 import * as implementation from "../src/api";
 
+// https://www.typescriptlang.org/docs/handbook/generics.html#generic-constraints
 type AssertIsSubSet<SUBSET extends SUPERSET, SUPERSET> = [SUBSET, SUPERSET];
 
 // TODO: export these helper types from test-utils
+// Because TypeScript uses a Structural Typing system
+// We can perform logical assertions on the API vs Impel
+// using discrete math semantics (subsets/supersets)
+// See: https://www.typescriptlang.org/docs/handbook/type-compatibility.html
 type EnsureAllDeclaredAreExposedAtRuntime = AssertIsSubSet<
   typeof implementation,
   typeof publicApi
