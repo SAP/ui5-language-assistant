8fa8abdd6fbba5bcea690820ac911782a98ad2d4
Tal Sapan <tal.sapan@sap.com>
2020-02-26 12:47:42 +0200

    chore(semantic-model): don't lint model ts files (#24)
    * Move the files to test-resources/ts-model under semantic-model
    * Exlude them in .eslintrc.json
    * Add script that compiles them (without emitting the output) to ensure
      the api json type definitions are correct and call it in semantic-model
      ci script
    * Changes in each model ts file: don't use relative reference to api.json
      type definitions, don't export the library variable (it isn't necessary
      anymore because the files aren't linted).

--- packages/semantic-model/test/1.60.14/input_ts/sap.ui.dt.designtime.api.ts
+++
@@ -1,7 +0,0 @@
-import { LibraryFile } from "../../../src/apiJson";
-
-export const api: LibraryFile = {
-  "$schema-ref": "http://schemas.sap.com/sapui5/designtime/api.json/1.0",
-  version: "1.60.14",
-  symbols: []
-};
