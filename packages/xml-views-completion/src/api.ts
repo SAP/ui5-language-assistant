4ea75c55c2f8ed44a3c0fb87fe29e0806543a070
Shachar Soel <shachar.soel@sap.com>
2020-02-27 13:24:56 +0200

    feat(xml-views-completion): filter suggestions by visibility (#28)

--- packages/xml-views-completion/src/api.ts
+++ packages/xml-views-completion/src/api.ts
@@ -1,12 +1,14 @@
 import { getSuggestions } from "@xml-tools/content-assist";
+import { filter, includes } from "lodash";
 
 import { GetXMLViewCompletionsOpts, XMLViewCompletion } from "../api";
 import { elementNameProviders } from "./providers/elementName";
+import { UI5Visibility } from "@vscode-ui5/semantic-model-types";
 
 export function getXMLViewCompletions(
   opts: GetXMLViewCompletionsOpts
 ): XMLViewCompletion[] {
-  return getSuggestions({
+  const suggestions = getSuggestions({
     offset: opts.offset,
     cst: opts.cst,
     ast: opts.ast,
@@ -19,4 +21,11 @@ export function getXMLViewCompletions(
       attributeValue: []
     }
   });
+
+  const allowedVisibility: UI5Visibility[] = ["public", "protected"];
+  const publicAndProtectedSuggestions = filter(suggestions, _ =>
+    includes(allowedVisibility, _.ui5Node.visibility)
+  );
+
+  return publicAndProtectedSuggestions;
 }
