cc00f46db01cf1f73d05d4962dcd8a97611d6ea6
Shachar Soel <shachar.soel@sap.com>
2020-03-04 15:48:47 +0200

    chore(xml-views-completion): remove unused code (#43)

--- packages/xml-views-completion/src/types.d.ts
+++
@@ -1,6 +0,0 @@
-import { ElementNameCompletionOptions } from "@xml-tools/content-assist";
-import { UI5SemanticModel } from "@vscode-ui5/semantic-model-types";
-
-export type UI5ElementNameCompletionOptions = ElementNameCompletionOptions<
-  UI5SemanticModel
->;
