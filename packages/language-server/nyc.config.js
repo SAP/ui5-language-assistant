3cd0a293f4c1ef2cfd6461208dfb29249d1a966a
rayakoren <raya.koren@sap.com>
2020-03-19 12:06:56 +0200

    improvement: remove from coverage

--- packages/language-server/nyc.config.js
+++ packages/language-server/nyc.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   reporter: ["text", "lcov"],
   "check-coverage": true,
-  exclude: [
-    "lib/server.js",
-    "lib/language-services.js",
-    "lib/api.js",
-    "lib/ui5-model.js"
-  ],
+  exclude: ["lib/server.js", "lib/language-services.js", "lib/ui5-model.js"],
   // https://reflectoring.io/100-percent-test-coverage/
   branches: 0,
   lines: 0,
---
+++ packages/vscode-ui5-language-support/test/extension.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+describe("the Language Server client", () => {
+  it("test", async () => {
+    expect(true).to.be.true;
+  });
+});
