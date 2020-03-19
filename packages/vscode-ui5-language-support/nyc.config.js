2d25bf9148fb9140072dba5c4e84462c9213f8b1
rayakoren <raya.koren@sap.com>
2020-03-19 11:11:37 +0200

    improvement: set coverage to 0

---
+++ packages/vscode-ui5-language-support/nyc.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reporter: ["text", "lcov"],
+  "check-coverage": true,
+  all: true,
+  // https://reflectoring.io/100-percent-test-coverage/
+  branches: 0,
+  lines: 0,
+  functions: 0,
+  statements: 0
+};
