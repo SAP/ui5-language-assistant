b914397af15cb3f702efb920d812204b983310a5
rayakoren <raya.koren@sap.com>
2020-03-19 11:56:10 +0200

    improvement: eclude files from coverage

--- packages/language-server/nyc.config.js
+++ packages/language-server/nyc.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   reporter: ["text", "lcov"],
   "check-coverage": true,
-  all: true,
+  exclude: [
+    "lib/server.js",
+    "lib/language-services.js",
+    "lib/api.js",
+    "lib/ui5-model.js"
+  ],
   // https://reflectoring.io/100-percent-test-coverage/
   branches: 0,
   lines: 0,
--- packages/vscode-ui5-language-support/nyc.config.js
+++ packages/vscode-ui5-language-support/nyc.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reporter: ["text", "lcov"],
   "check-coverage": true,
-  all: true,
+  exclude: ["lib/extension.js"],
   // https://reflectoring.io/100-percent-test-coverage/
   branches: 0,
   lines: 0,
