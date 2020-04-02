f75a43f7430a55fd26a7b27b06e2bf73cc26874b
Tal Sapan <tal.sapan@sap.com>
2020-04-02 12:10:48 +0300

    test(test-utils): cr fixes
    Download metadata.json from public npm registry instead of using local file
    Use async fs-extra APIs

--- test-packages/test-utils/src/utils/download-ui5-resources.ts
+++ test-packages/test-utils/src/utils/download-ui5-resources.ts
@@ -4,53 +4,22 @@
 import { TestModelVersion } from "../../api";
 import { reduce, forEach, keys } from "lodash";
 import { dirname, resolve } from "path";
-import { writeFileSync, mkdirsSync, readFileSync } from "fs-extra";
+import { writeFile, mkdirs } from "fs-extra";
 import fetch from "node-fetch";
 
-async function getLibs(): Promise<string[]> {
-  // To fetch the metadata.json file dynamically by version do the following:
-  // 1. Add these devDependencies in the package.json:
-  //     "jszip": "3.2.2",
-  //     "@types/jszip": "3.1.7"
-  // Note: this requires the typescript compiler to be aware of the dom types,
-  // which is done by adding "dom" to "lib" in tsconfig.base.json
-  //
-  // 2. Add this import:
-  // import { loadAsync } from "jszip";
-  //
-  // 3. Add parameter "version: TestModelVersion" and pass the version where this function is called
-  //
-  // 4. Uncomment the following code:
+async function getLibs(version: TestModelVersion): Promise<string[]> {
   // The metadata.json seems to have been added only very recently :(
-  // let versionInURL: string = version;
-  // if (versionInURL !== "1.75.0") {
-  //   versionInURL = "1.75.0";
-  // }
-  // const url = `http://nexusrel.wdf.sap.corp:8081/nexus/service/local/repositories/deploy.releases/content/com/sap/ui5/dist/sapui5-dist/${versionInURL}/sapui5-dist-${versionInURL}-npm-sources.zip`;
-  // const response = await fetch(url);
-  // if (!response.ok) {
-  //   console.log(`error fetching from ${url}`);
-  //   return [];
-  // }
-  //
-  // const zipResponse = await response.buffer();
-  // const zip = await loadAsync(zipResponse);
-  // const fileContent = await zip.file("metadata.json").async("string");
-  //
-  // 5. Remove the following code (until the definition of fileContent)
-  const pkgJsonPath = require.resolve(
-    "@ui5-language-assistant/test-utils/package.json"
-  );
-  const rootPkgFolder = dirname(pkgJsonPath);
-  // The content of this file was copied from the URL above (for version 1.75.0), from file metadata.json inside the zip
-  const metadataFile = resolve(
-    rootPkgFolder,
-    "resources",
-    "model",
-    "metadata.json"
-  );
-  const fileContent = readFileSync(metadataFile, "UTF-8");
-
+  let versionInURL: string = version;
+  if (versionInURL !== "1.76.0") {
+    versionInURL = "1.76.0";
+  }
+  const url = `https://unpkg.com/@sapui5/distribution-metadata@${versionInURL}/metadata.json`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    console.log(`error fetching from ${url}`);
+    return [];
+  }
+  const fileContent = await response.text();
   const librariesMetadata = JSON.parse(fileContent);
   return keys(librariesMetadata.libraries);
 }
@@ -63,7 +32,7 @@ export async function addUi5Resources(
   // Older versions:
   // https://sapui5.hana.ondemand.com/1.71.14/test-resources/sap/m/designtime/api.json
   const baseUrl = `https://sapui5.hana.ondemand.com/${version}/test-resources/`;
-  const libs = await getLibs();
+  const libs = await getLibs(version);
   const nameToFile = reduce(
     libs,
     (nameToFile, lib) => {
@@ -87,7 +56,7 @@ export async function addUi5Resources(
     version,
     "input"
   );
-  mkdirsSync(modelFolder);
+  await mkdirs(modelFolder);
   forEach(nameToFile, async (url, name) => {
     await writeUrlToFile(
       url,
@@ -109,7 +78,7 @@ async function writeUrlToFile(url: string, file: string): Promise<void> {
     console.log(`empty object returned from ${url}`);
     return;
   }
-  writeFileSync(file, text);
+  await writeFile(file, text);
 }
 
 addUi5Resources("1.71.14");
