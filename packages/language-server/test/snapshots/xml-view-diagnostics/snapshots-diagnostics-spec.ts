b372a93cd825433751609de26d29a767dea8a192
Tal Sapan <tal.sapan@sap.com>
2020-05-11 18:25:07 +0300

    refactor: auto-generate snapshot test case from folder

---
+++ packages/language-server/test/snapshots/xml-view-diagnostics/snapshots-diagnostics-spec.ts
@@ -0,0 +1,17 @@
+import * as klawSync from "klaw-sync";
+import { map, forEach } from "lodash";
+import { basename } from "path";
+import { snapshotTestLSPDiagnostic } from "./snapshots-utils";
+
+describe(`The language server diagnostics capability`, () => {
+  const snapshotTestsDir = __dirname;
+  const klawItems = klawSync(snapshotTestsDir, { depthLimit: 1, nofile: true });
+  const testDirs = map(klawItems, (_) => _.path);
+
+  forEach(testDirs, (dirPath) => {
+    const dirName = basename(dirPath);
+    it("Can create diagnostic for " + dirName.replace(/-/g, " "), async () => {
+      await snapshotTestLSPDiagnostic(dirPath);
+    });
+  });
+});
