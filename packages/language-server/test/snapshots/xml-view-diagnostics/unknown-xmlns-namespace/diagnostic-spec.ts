3c3733898e298c2f26bf2577a9f8ced3d17662b7
Tal Sapan <tal.sapan@sap.com>
2020-05-13 09:18:38 +0300

    test: remove unnecessary describe for snapshot test
    They are now generated automatically

--- packages/language-server/test/snapshots/xml-view-diagnostics/unknown-xmlns-namespace/diagnostic-spec.ts
+++
@@ -1,6 +0,0 @@
-import { snapshotTestLSPDiagnostic } from "../snapshots-utils";
-describe(`The language server diagnostics capability`, () => {
-  it("Can create diagnostic for an unknown namespace in xmlns attribute value", async () => {
-    await snapshotTestLSPDiagnostic(__dirname);
-  });
-});
