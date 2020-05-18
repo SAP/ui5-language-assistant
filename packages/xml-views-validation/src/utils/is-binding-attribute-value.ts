b4ac8ae44dd97bcdea2bf26ac55f888ffc817206
tal-sapan <tal.sapan@sap.com>
2020-05-18 09:45:53 +0300

    fix: use more accurate check for binding expression (#123)

--- packages/xml-views-validation/src/utils/is-binding-attribute-value.ts
+++ packages/xml-views-validation/src/utils/is-binding-attribute-value.ts
@@ -1,7 +1,9 @@
 export function isPossibleBindingAttributeValue(value: string): boolean {
   // Check if an attribute value might be a binding expression.
-  // The current check is very naive - it only looks for matching curly brackets anywhere in the value.
-  // In the future we might perform a more extensive check, but this is enough for now, so we don't
-  // return false positives in attribute value validations.
-  return /{(.*)}/.test(value);
+  // The current check doesn't diffrenciate between attribute types, although for non-string
+  // attributes the check can be simpler.
+  // Note that the syntax might not be correct (e.g. there is no check for balanced brackets)
+  // but the runtime will still treat it as a binding expression even in that case (it will throw an error).
+  // This will match everything that contains an un-escaped { character.
+  return /^(?:[^\\]|\\.)*{/.test(value);
 }
