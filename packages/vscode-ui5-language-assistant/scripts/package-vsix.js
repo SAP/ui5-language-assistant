025fe20062c96f7294a7feee328f1f67b2b8211c
Shahar Soel <shachar.soel@sap.com>
2020-06-17 18:54:41 +0300

    chore: cr fix

--- packages/vscode-ui5-language-assistant/scripts/package-vsix.js
+++ packages/vscode-ui5-language-assistant/scripts/package-vsix.js
@@ -14,7 +14,7 @@
 const proxyquire = require("proxyquire");
 const { expect } = require("chai");
 const { resolve } = require("path");
-const { cloneDeep, forEach } = require("lodash");
+const { forEach } = require("lodash");
 const { readFileSync, writeFileSync } = require("fs");
 const { writeJsonSync } = require("fs-extra");
 
@@ -62,15 +62,14 @@ const pkgJsonPath = resolve(rootPkgDir, "package.json");
 // Read & save the original literal representation of the pkg.json
 // To avoid dealing with re-formatting (prettier) later on.
 const pkgJsonOrgStr = readFileSync(pkgJsonPath, "utf8");
-const pkgJsonOrg = JSON.parse(pkgJsonOrgStr);
+const pkgJson = JSON.parse(pkgJsonOrgStr);
 // During development flows the `main` should point to the compiled sourced
 // for fast dev feedback loops.
-expect(pkgJsonOrg.main).to.equal("./lib/src/extension");
-const pkgJsonBundled = cloneDeep(pkgJsonOrg);
+expect(pkgJson.main).to.equal("./lib/src/extension");
 // During production flows the main should point to the bundled sources
 // to reduce loading time.
-pkgJsonBundled.main = "./dist/extension";
-writeJsonSync(pkgJsonPath, pkgJsonBundled, { spaces: 2, EOF: "\n" });
+pkgJson.main = "./dist/extension";
+writeJsonSync(pkgJsonPath, pkgJson, { spaces: 2, EOF: "\n" });
 
 packageCommand({
   cwd: rootPkgDir,
