baf6e0155775e7dd80f59140af37b21c22c822b9
Shahar Soel <4233843+bd82@users.noreply.github.com>
2020-12-17 19:46:38 +0200

    test: disable vscode tests (#343)
    See details in #342

--- packages/vscode-ui5-language-assistant/test/run-integration-tests.ts
+++ packages/vscode-ui5-language-assistant/test/run-integration-tests.ts
@@ -1,4 +1,5 @@
 import { resolve } from "path";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { runTests } from "vscode-test";
 import globby from "globby";
 
@@ -15,10 +16,13 @@ async function main(): Promise<void> {
     const scenarioPaths = await globby(`${testPkgFolder}/**/index.js`);
     // Use for of + await to ensure running in sequence because vscode-test library cannot start multiple vscode instances at the same time
     for (const path of scenarioPaths) {
-      await runTests({
-        extensionDevelopmentPath,
-        extensionTestsPath: path,
-      });
+      console.warn(
+        `SKIPPING TEST: ${path}.\nsee: https://github.com/SAP/ui5-language-assistant/issues/342`
+      );
+      // await runTests({
+      //   extensionDevelopmentPath,
+      //   extensionTestsPath: path,
+      // });
     }
   } catch (err) {
     console.error("Failed to run tests: ", err);
