a68e69f494c1366ea75a147c9325a24d23ee3365
Shahar Soel <shachar.soel@sap.com>
2020-12-30 16:16:46 +0200

    docs: fix english grammar

--- packages/language-server/api.d.ts
+++ packages/language-server/api.d.ts
@@ -22,7 +22,7 @@ export type ServerInitializationOptions = {
   /**
    * Initial logging level for the language server.
    * The log level may be changed **after** the server has started
-   * By send a custom `changeLogLevel` LSP request
+   * By sending a custom `changeLogLevel` LSP request
    */
   logLevel?: LogLevel;
 };
