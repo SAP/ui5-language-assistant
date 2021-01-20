1fb50ebf3a336b391fc2d346cb126d22ad8668e5
Shahar Soel <4233843+bd82@users.noreply.github.com>
2021-01-20 10:37:43 +0200

    build(deps-dev): bump multiple dev-deps (#368)

--- packages/semantic-model/test/implementation-matches-public-api.ts
+++ packages/semantic-model/test/implementation-matches-public-api.ts
@@ -9,11 +9,13 @@ type AssertIsSubSet<SUBSET extends SUPERSET, SUPERSET> = [SUBSET, SUPERSET];
 // We can perform logical assertions on the API vs Impel
 // using discrete math semantics (subsets/supersets)
 // See: https://www.typescriptlang.org/docs/handbook/type-compatibility.html
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type EnsureAllDeclaredAreExposedAtRuntime = AssertIsSubSet<
   typeof implementation,
   typeof publicApi
 >;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type EnsureNoRedundantAreExposedAtRuntime = AssertIsSubSet<
   typeof publicApi,
   typeof implementation
