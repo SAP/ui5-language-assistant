d0874336158ad8723f241a4273f368a451cff3ec
C5226337 <davis.voicescuks@sap.com>
2022-11-28 17:39:08 +0200

    test: increase timeout

--- .mocharc.js
+++ .mocharc.js
@@ -8,5 +8,5 @@ module.exports = {
   require: ["source-map-support/register"],
   spec: "./lib/test/**/*spec.js",
   // we have many async tests which depend on lazily generating the model
-  timeout: 8000,
+  timeout: 10000,
 };
