841e9acf372e4bb4caee3ff024151988d2a95ad2
Vadim Koblov <vadims.koblovs@sap.com>
2023-01-16 15:16:14 +0200

    fix: pr comments

--- packages/context/src/fetch.ts
+++ packages/context/src/fetch.ts
@@ -2,6 +2,38 @@ import { getProxyForUrl } from "proxy-from-env";
 import HttpsProxyAgent from "https-proxy-agent";
 import { RequestInfo, RequestInit, Response } from "node-fetch";
 
+export type FetcherType = (
+  url: RequestInfo,
+  init?: RequestInit | undefined
+) => Promise<Response>;
+
+/**
+ * Promise which prepares dynamic node fetcher function and holds it within once resolved
+ */
+const nodeFetchCached = new Promise<FetcherType>((done, reject) => {
+  (async () => {
+    let nodeFetch: FetcherType;
+    try {
+      // this should work when bundled
+      nodeFetch = (await import("node-fetch")).default;
+    } catch (e) {
+      // trying to load dynamically
+      const importDynamic = new Function(
+        "modulePath",
+        "return import(modulePath)"
+      );
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      nodeFetch = async (...args: any[]) => {
+        const module = await importDynamic("node-fetch");
+        return module.default(...args);
+      };
+    }
+    return nodeFetch;
+  })()
+    .then((result) => done(result))
+    .catch((error) => reject(error));
+});
+
 /**
  * Wrapper for the node-fetch API to utilize a proxy if needed
  * @param url the url to call
@@ -22,22 +54,7 @@ export default async function fetch(
     });
   }
 
-  // call the node-fetch API
-  let nodeFetch;
-  try {
-    // this should work when bundled
-    nodeFetch = (await import("node-fetch")).default;
-  } catch (e) {
-    // trying to load dynamically
-    const importDynamic = new Function(
-      "modulePath",
-      "return import(modulePath)"
-    );
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    nodeFetch = async (...args: any[]) => {
-      const module = await importDynamic("node-fetch");
-      return module.default(...args);
-    };
-  }
+  // call the cached node-fetch API
+  const nodeFetch = await nodeFetchCached;
   return nodeFetch(url, init);
 }
