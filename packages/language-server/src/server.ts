8060f6c075fb3d318b2cf584fddf1b1197113098
mr25mr <100434800+mr25mr@users.noreply.github.com>
2023-01-24 11:33:57 +0100

    fix: clear diagnostics for a closed file (#539)
    * fix: clear diagnostics for a closed file
    
    * fix: resue variable

--- packages/language-server/src/server.ts
+++ packages/language-server/src/server.ts
@@ -346,7 +346,12 @@ documents.onDidClose((textDocumentChangeEvent) => {
   getLogger().debug("`onDidClose` event", {
     textDocumentChangeEvent,
   });
-  clearDocumentSettings(textDocumentChangeEvent.document.uri);
+  const uri = textDocumentChangeEvent.document.uri;
+  if (isXMLView(uri)) {
+    // clear diagnostics for a closed file
+    connection.sendDiagnostics({ uri, diagnostics: [] });
+  }
+  clearDocumentSettings(uri);
 });
 
 documents.listen(connection);
