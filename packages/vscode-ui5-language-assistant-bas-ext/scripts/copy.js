865dab18aa4011f65c95cb5bea4b67baf337384d
Maruf Rasully <maruf.rasully@sap.com>
2023-01-25 13:02:51 +0100

    fix: review comments

--- packages/vscode-ui5-language-assistant-bas-ext/scripts/copy.js
+++ packages/vscode-ui5-language-assistant-bas-ext/scripts/copy.js
@@ -1,30 +1,32 @@
 const { copySync, readdirSync } = require("fs-extra");
 const { join } = require("path");
 
-const getBaseSrc = () =>
-  join(__dirname, "..", "..", "vscode-ui5-language-assistant");
-const getBaseDes = () => join(__dirname, "..");
-
-const srcReadme = join(getBaseSrc(), "README.md");
-const destinationReadme = join(getBaseDes(), "README.md");
-console.log(`Copying from ${srcReadme} to ${destinationReadme}`);
-copySync(srcReadme, destinationReadme);
-console.log("Copying finished successfully");
+const baseSrc = join(__dirname, "..", "..", "vscode-ui5-language-assistant");
+const baseDes = join(__dirname, "..");
 
 const vsixFiles = [];
-readdirSync(getBaseSrc()).forEach((item) => {
+readdirSync(baseSrc).forEach((item) => {
   if (item.endsWith(".vsix")) {
     vsixFiles.push(item);
     return item;
   }
 });
+if (vsixFiles.length > 1) {
+  throw new Error(
+    `Detected more than one ".vsix" files. There should be only one ".vsix" file. Please cross check and try again.`,
+    { cause: vsixFiles }
+  );
+}
 const vsixFile = vsixFiles.pop();
 if (!vsixFile) {
-  console.log(`There is not vsix under ${getBaseSrc()}`);
-  return;
+  console.log(`There is not vsix under ${baseSrc}`);
+  throw new Error(
+    `There is no ".vsix" file. Please make sure a recent ".vsix" file is build under ${baseSrc} and try again.`,
+    { cause: vsixFiles }
+  );
 }
-const srcVsix = join(getBaseSrc(), vsixFile);
-const destinationVsix = join(getBaseDes(), vsixFile);
+const srcVsix = join(baseSrc, vsixFile);
+const destinationVsix = join(baseDes, vsixFile);
 console.log(`Copying from ${srcVsix} to ${destinationVsix}`);
 copySync(srcVsix, destinationVsix);
 console.log("Copying finished successfully");
