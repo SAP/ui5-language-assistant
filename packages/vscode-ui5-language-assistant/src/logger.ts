166fb5d01bc3bef16e58a2094b61d908b6e9abd0
mr25mr <100434800+mr25mr@users.noreply.github.com>
2023-03-28 09:26:16 +0200

    fix: replace webpack with esbuild which solved crashing (#581)
    * fix: replace webpack with esbuild which solved crashing
    
    * fix: changeset is added and minor corrections
    
    * fix: esbuild scripts optimization
    
    * fix: enable github action build job for pull requests
    
    * fix: change set update
    
    * fix: fixed server crash with trace log level, node version update for esbuild
    
    * fix: webpack configs removed
    
    ---------
    
    Co-authored-by: Vadim Koblov <vadims.koblovs@sap.com>

--- packages/vscode-ui5-language-assistant/src/logger.ts
+++ packages/vscode-ui5-language-assistant/src/logger.ts
@@ -2,11 +2,18 @@ import {
   getLogger as logger,
   ILogger,
 } from "@ui5-language-assistant/logic-utils";
+import { join } from "path";
+import { readJsonSync } from "fs-extra";
 
 const getPackageName = (): string => {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires -- Using `require` for .json file as this gets bundled with webpack correctly.
-  const meta = require("../../package.json");
-  return meta.name;
+  let meta;
+  try {
+    // in production - ../extensions/saposs.vscode-ui5-language-assistant-4.0.6/dist which equals to vscode-ui5-language-assistant/dist
+    meta = readJsonSync(join(__dirname, "..", "package.json"));
+  } catch (error) {
+    meta = readJsonSync(join(__dirname, "..", "..", "package.json"));
+  }
+  return meta?.name;
 };
 
 const name = getPackageName();
