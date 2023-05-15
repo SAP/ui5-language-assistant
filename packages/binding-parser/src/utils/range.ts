40ffbe6491f33c5c62ac77452f29f3ca0fac4080
Maruf Rasully <maruf.rasully@sap.com>
2023-05-15 08:52:28 +0300

    fix: some issues

--- packages/binding-parser/src/utils/range.ts
+++ packages/binding-parser/src/utils/range.ts
@@ -1,76 +1,58 @@
 import { IToken, CstNodeLocation } from "chevrotain";
-import type { Position, Range } from "vscode-languageserver-types";
-export const getRange = (token: IToken, position?: Position): Range => {
-  if (position) {
-    const start = {
-      line: token.startLine
-        ? token.startLine - 1 + position.line
-        : position.line,
-      character: token.startColumn
-        ? token.startColumn -
-          1 +
-          (token.startLine && token.startLine > 1 ? 0 : position.character)
-        : position.character,
-    };
-    const end = {
-      line: token.endLine ? position.line + (token.endLine - 1) : position.line,
-      character: token.endColumn
-        ? token.endColumn +
-          (token.endLine && token.endLine > 1 ? 0 : position.character)
-        : position.character,
-    };
-    return {
-      start,
-      end,
-    };
+import { Position, Range } from "vscode-languageserver-types";
+const hasNaNOrUndefined = (value: undefined | number): boolean => {
+  if (value === undefined) {
+    return true;
   }
-  return {
-    start: {
-      line: token.startLine ? token.startLine - 1 : 0,
-      character: token.startColumn ? token.startColumn - 1 : 0,
-    },
-    end: {
-      line: token.endLine ? token.endLine - 1 : 0,
-      character: token.endColumn ?? 0,
-    },
-  };
+  return isNaN(value);
 };
+const adjustPosition = (position: Position, offset: Position): Position =>
+  Position.create(
+    position.line + offset.line,
+    position.line === 0
+      ? position.character + offset.character
+      : position.character
+  );
+
+export const getRange = (token: IToken, startPosition?: Position): Range => {
+  const start = Position.create(
+    hasNaNOrUndefined(token.startLine) ? 0 : token.startLine! - 1,
+    hasNaNOrUndefined(token.startColumn) ? 0 : token.startColumn! - 1
+  );
+  const line = hasNaNOrUndefined(token.endLine) ? 0 : token.endLine! - 1;
+
+  const char = hasNaNOrUndefined(token.endColumn) ? 0 : token.endColumn!;
+
+  const end = Position.create(line, char);
+
+  if (startPosition) {
+    return Range.create(
+      adjustPosition(start, startPosition),
+      adjustPosition(end, startPosition)
+    );
+  }
+
+  return Range.create(start, end);
+};
+
 export const locationToRange = (
   location?: CstNodeLocation,
-  position?: Position
+  startPosition?: Position
 ): Range | undefined => {
   if (!location) {
     return;
   }
-  if (position) {
-    const start = {
-      line: location.startLine
-        ? location.startLine - 1 + position.line
-        : position.line,
-      character: location.startColumn
-        ? location.startColumn - 1 + position.character
-        : position.character,
-    };
-    return {
-      start,
-      end: {
-        line: location.endLine
-          ? position.line + (location.endLine - 1)
-          : position.line,
-        character: location.endColumn
-          ? location.endColumn + position.character
-          : position.character,
-      },
-    };
+  const range = Range.create(
+    hasNaNOrUndefined(location.startLine) ? 0 : location.startLine! - 1,
+    hasNaNOrUndefined(location.startColumn) ? 0 : location.startColumn! - 1,
+    hasNaNOrUndefined(location.endLine) ? 0 : location.endLine! - 1,
+    hasNaNOrUndefined(location.endColumn) ? 0 : location.endColumn!
+  );
+  if (startPosition) {
+    return Range.create(
+      adjustPosition(range.start, startPosition),
+      adjustPosition(range.end, startPosition)
+    );
   }
-  return {
-    start: {
-      line: location.startLine ? location.startLine - 1 : 0,
-      character: location.startColumn ? location.startColumn - 1 : 0,
-    },
-    end: {
-      line: location.endLine ? location.endLine - 1 : 0,
-      character: location.endColumn ?? 0, // maybe plus 1?
-    },
-  };
+  return range;
 };
--- packages/binding/test/utils/expression-spec.ts
+++ packages/binding/test/utils/expression-spec.ts
@@ -4,6 +4,8 @@ import {
   isBindingExpression,
   isPropertyBindingInfo,
 } from "../../src/utils";
+import { parsePropertyBindingInfo } from "@ui5-language-assistant/binding-parser";
+
 describe("expression", () => {
   context("isBindingExpression", () => {
     it("check binding expression {=", () => {
@@ -19,45 +21,45 @@ describe("expression", () => {
       expect(result).to.be.false;
     });
   });
-  context("extractBindingExpression", () => {
+  context.only("extractBindingExpression", () => {
     it("text and binding syntax", () => {
       const input = 'some text here {path: "some/path"} some text here too';
       const result = extractBindingExpression(input);
-      expect(result).to.deep.equal({
+      expect(result).to.deep.equal([{
         startIndex: 15,
         endIndex: 33,
         expression: '{path: "some/path"}',
-      });
+      }]);
     });
     it("text with escaped brackets and binding syntax", () => {
       const input =
         'some text \\{ and \\[ some \\} text \\] here too and here too {path: "some/path"} some text \\{ and \\[ some \\} text \\] here too';
       const result = extractBindingExpression(input);
-      expect(result).to.deep.equal({
+      expect(result).to.deep.equal([{
         startIndex: 58,
         endIndex: 76,
         expression: '{path: "some/path"}',
-      });
+      }]);
     });
     it("binding syntax only [missing closing curly bracket]", () => {
       const input = '{path: "test-value", ,   ';
       const result = extractBindingExpression(input);
-      expect(result).to.deep.equal({
+      expect(result).to.deep.equal([{
         startIndex: 0,
         endIndex: 25,
         expression: '{path: "test-value", ,   ',
-      });
+      }]);
     });
     it("binding syntax only [simple]", () => {
       const input = "{events: {a: {}}, {}}";
       const result = extractBindingExpression(input);
-      expect(result).to.deep.equal({
+      expect(result).to.deep.equal([{
         startIndex: 0,
         endIndex: 20,
         expression: "{events: {a: {}}, {}}",
-      });
+      }]);
     });
-    it("binding syntax only", () => {
+    it.only("binding syntax only", () => {
       const input = `{
         path:'/Travel',
         parameters : {
@@ -66,47 +68,82 @@ describe("expression", () => {
         }
 			}`;
       const result = extractBindingExpression(input);
-      expect(result).to.deep.equal({
+      expect(result).to.deep.equal([{
         startIndex: 0,
         endIndex: 221,
         expression: input,
-      });
+      }]);
+    });
+    it("text and multi binding syntaxes", () => {
+      const input =
+        "some text here {path:'gender', formatter:'.myGenderFormatter'} some in between {firstName}, {lastName} some here too";
+      const result = extractBindingExpression(input);
+      expect(result).to.deep.equal([
+        {
+          endIndex: 62,
+          expression: "{path:'gender', formatter:'.myGenderFormatter'}",
+          startIndex: 15,
+        },
+        {
+          endIndex: 90,
+          expression: "{firstName}",
+          startIndex: 79,
+        },
+        {
+          endIndex: 102,
+          expression: "{lastName}",
+          startIndex: 92,
+        },
+      ]);
     });
   });
   context("isPropertyBindingInfo", () => {
     it("empty string", () => {
       const input = "  ";
-      const result = isPropertyBindingInfo(input);
+      const { ast } = parsePropertyBindingInfo(input);
+      const result = isPropertyBindingInfo(ast, input);
       expect(result).to.be.true;
+    });
+    it("string value", () => {
+      const input = "40";
+      const { ast } = parsePropertyBindingInfo(input);
+      const result = isPropertyBindingInfo(ast, input);
+      expect(result).to.be.false;
     });
     it("empty curly bracket without space", () => {
       const input = "{}";
-      const result = isPropertyBindingInfo(input);
+      const { ast } = parsePropertyBindingInfo(input);
+      const result = isPropertyBindingInfo(ast, input);
       expect(result).to.be.true;
     });
     it("empty curly bracket with space", () => {
       const input = "{   }";
-      const result = isPropertyBindingInfo(input);
+      const { ast } = parsePropertyBindingInfo(input);
+      const result = isPropertyBindingInfo(ast, input);
       expect(result).to.be.true;
     });
     it("key with colone [true]", () => {
       const input = ' {path: "some/path"}';
-      const result = isPropertyBindingInfo(input);
+      const { ast } = parsePropertyBindingInfo(input);
+      const result = isPropertyBindingInfo(ast, input);
       expect(result).to.be.true;
     });
     it("key with colone any where [true]", () => {
       const input = ' {path "some/path", thisKey: {}}';
-      const result = isPropertyBindingInfo(input);
+      const { ast } = parsePropertyBindingInfo(input);
+      const result = isPropertyBindingInfo(ast, input);
       expect(result).to.be.true;
     });
     it("missing colon [false]", () => {
       const input = '{path "some/path"}';
-      const result = isPropertyBindingInfo(input);
+      const { ast } = parsePropertyBindingInfo(input);
+      const result = isPropertyBindingInfo(ast, input);
       expect(result).to.be.false;
     });
     it("contains > [false]", () => {
       const input = "{i18n>myTestModel}";
-      const result = isPropertyBindingInfo(input);
+      const { ast } = parsePropertyBindingInfo(input);
+      const result = isPropertyBindingInfo(ast, input);
       expect(result).to.be.false;
     });
   });
