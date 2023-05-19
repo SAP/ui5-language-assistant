2a20461c27b7917382ca2fa41f33cb1673a1dd31
Vadim Koblov <vadims.koblovs@sap.com>
2023-05-19 14:57:20 +0300

    fix: work on pr comments

--- packages/logic-utils/jest.config.js
+++ packages/logic-utils/jest.config.js
@@ -1,17 +1,8 @@
 const { join } = require("path");
+const defaultConfig = require("../../jest.config");
 
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  automock: false,
-  errorOnDeprecated: true,
-  notify: false,
-  notifyMode: "failure",
-  verbose: false,
-  testMatch: ["**/unit/**/?(*.)+(test).ts"],
-  transform: {
-    "^.+\\.ts?$": "ts-jest",
-  },
+  ...defaultConfig,
   globals: {
     "ts-jest": {
       tsconfig: join(__dirname, "tsconfig-test.json"),
@@ -21,43 +12,4 @@ module.exports = {
       },
     },
   },
-  testResultsProcessor: "jest-sonar-reporter",
-  collectCoverage: true,
-  snapshotFormat: {
-    escapeString: true,
-    printBasicPrototype: true,
-  },
-  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
-  coverageDirectory: "reports/test/unit/coverage",
-  coverageThreshold: {
-    global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
-    },
-  },
-
-  modulePathIgnorePatterns: [
-    "<rootDir>/dist/",
-    "<rootDir>/node_modules/",
-    "<rootDir>/test/unit/samples/",
-    "<rootDir>/test/int/test-data/",
-    "<rootDir>/test/int/test-data-copy/",
-  ],
-  testPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/dist/",
-    "<rootDir>/lib/",
-  ],
-  transformIgnorePatterns: ["<rootDir>/node_modules/", "/node_modules/"],
-  reporters: [
-    "default",
-    "summary",
-    [
-      "jest-junit",
-      { outputDirectory: "reports/test/unit", outputName: "junit-report.xml" },
-    ],
-  ],
-  setupFilesAfterEnv: ["./jest.setup.js"],
 };
