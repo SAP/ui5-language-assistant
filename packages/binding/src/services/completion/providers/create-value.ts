1de7540debd79ced211fb13d154f289e22040795
Maruf Rasully <maruf.rasully@sap.com>
2023-06-26 08:19:07 +0200

    refactor: create value

--- packages/binding/src/services/completion/providers/create-value.ts
+++ packages/binding/src/services/completion/providers/create-value.ts
@@ -3,42 +3,13 @@ import {
   CompletionItemKind,
   InsertTextFormat,
 } from "vscode-languageserver-types";
-import {
-  isCollectionValue,
-  isStructureValue,
-  positionContained,
-  isBefore,
-  BindingParserTypes as BindingTypes,
-  rangeContained,
-} from "@ui5-language-assistant/binding-parser";
+import { BindingParserTypes as BindingTypes } from "@ui5-language-assistant/binding-parser";
 
 import { getPropertyBindingInfoElements } from "../../../definition/definition";
-import { isParts, typesToValue } from "../../../utils";
-import { getCompletionItems } from "./property-binding-info";
+import { typesToValue } from "../../../utils";
 import { BindContext, ValueContext } from "../../../types";
 import { createDefaultValue } from "./create-default-value";
-
-const getCollectionCompletionItem = (
-  context: BindContext,
-  element: BindingTypes.StructureElement
-): CompletionItem[] => {
-  const bindingElement = getPropertyBindingInfoElements(context).find(
-    (el) => el.name === (element.key && element.key.text)
-  );
-  const data = typesToValue(
-    /* istanbul ignore next */
-    bindingElement?.type ?? [],
-    context,
-    0,
-    true
-  );
-  return data.map((item) => ({
-    label: item.replace(/\$\d+/g, ""),
-    insertTextFormat: InsertTextFormat.Snippet,
-    insertText: item,
-    kind: CompletionItemKind.Field,
-  }));
-};
+import { createCollectionValue } from "./create-collection-value";
 
 export const createValue = (
   context: BindContext,
@@ -68,42 +39,6 @@ export const createValue = (
   }
 
   completionItems.push(...createDefaultValue(context, valueContext));
-  if (isCollectionValue(element.value) && isParts(element)) {
-    /* istanbul ignore next */
-    const position = context.textDocumentPosition?.position;
-    if (!position) {
-      return completionItems;
-    }
-    const el = element.value.elements
-      .filter((item) => !!item)
-      .find((item) => positionContained(item.range, position));
-
-    if (isStructureValue(el)) {
-      if (
-        (el.leftCurly && isBefore(position, el.leftCurly.range.start, true)) ||
-        (el.rightCurly && isBefore(el.rightCurly.range.end, position, true))
-      ) {
-        // position is outside {}
-        return getCollectionCompletionItem(context, element);
-      }
-      const result = getCompletionItems(context, el, spaces).filter(
-        (item) => item.label !== "parts"
-      );
-      return result;
-    }
-    if (
-      element.value.range &&
-      (isBefore(position, element.value.range.start, true) ||
-        isBefore(element.value.range.end, position, true))
-    ) {
-      // position is outside []
-      return completionItems;
-    }
-    if (el && rangeContained(el.range, { start: position, end: position })) {
-      // on primitive value e.g '|'
-      return completionItems;
-    }
-    return getCollectionCompletionItem(context, element);
-  }
+  completionItems.push(...createCollectionValue(context, spaces, valueContext));
   return completionItems;
 };
